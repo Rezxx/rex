cb12b65a14c406c28b31a1624717ef2d252d0c19
Rex Shen <rex0481775250@gmail.com>
2020-03-04 20:21:17 +1100

    add tags

--- src/Project.js
+++ src/Project.js
@@ -1,9 +1,12 @@
 import React, {Component} from 'react';
 import {motion } from "framer-motion"
 import Grid from '@material-ui/core/Grid';
+import {Tag} from 'antd';
 
 import Layout from './Layout';
 import face from './img/face.jpg'
+
+import 'antd/lib/tag/style/index.css';
 
 const move = 100;
 
@@ -20,13 +23,24 @@ const rightVariants = {
 };
 
 
-const projects = [{title: 'Connect Four Solver', img: face, url: 'https://github.com/Rezxx/ConnectFour_Solver', desc: 'Offers a best move in a ConnectFour game using Alpha-Beta pruning MiniMax algo.'}
-             ,{title: 'Face Recognition', img: 'https://media.giphy.com/media/Kbwl4xM4ZhL1KD9RHd/giphy.gif', url: 'https://github.com/Rezxx/Face-Recognition', desc: 'Face recognition tool uses EigenFace and k-NN classifier'}
-             ,{title: 'Throw Balls',img: 'https://media.giphy.com/media/L0xUOFkNYvZysYkOOv/giphy.gif', url: 'https://github.com/Rezxx/Throw-Balls', desc:'A ball throwing game built using PyGame and OpenGL'}
-             ,{title: 'Pool Game', img: face, url: 'https://github.com/Rezxx/PoolGame', desc:'Virtual pool game under Qt enviornment'}
-             ,{title: 'Bird',img: 'https://media.giphy.com/media/cMEcAd8nS8UAQAdtLi/giphy.gif', url: 'https://github.com/Rezxx/Bird', desc: 'Web music player based on Django and React'}
-             ,{title: 'Digger',img: face, url: 'https://bitbucket.org/Rex9611/info3600-bugchecker/src/master/', desc: 'Static analysis tool finding Java Null Pointer Exceptions based on Soufflé and Doop'}
-             ,{title: 'ClearPay',img: face, url: '#', desc:'An Android App for splitting bill between users, supporting offline transactions via Bluetooth.'}];
+const projects = [{title: 'Connect Four Solver',stack:['Java'], img: face, url: 'https://github.com/Rezxx/ConnectFour_Solver', desc: 'Offers a best move in a ConnectFour game using Alpha-Beta pruning MiniMax algo.'}
+             ,{title: 'Face Recognition', stack:['Python', 'OpenCV'], img: 'https://media.giphy.com/media/Kbwl4xM4ZhL1KD9RHd/giphy.gif', url: 'https://github.com/Rezxx/Face-Recognition', desc: 'Face recognition tool uses EigenFace and k-NN classifier'}
+             ,{title: 'Throw Balls', stack:['Python', 'PyGame', 'OpenGL'] ,img: 'https://media.giphy.com/media/L0xUOFkNYvZysYkOOv/giphy.gif', url: 'https://github.com/Rezxx/Throw-Balls', desc:'A ball throwing game built using PyGame and OpenGL'}
+             ,{title: 'Pool Game', stack:['C++', 'Qt'], img: face, url: 'https://github.com/Rezxx/PoolGame', desc:'Virtual pool game under Qt enviornment'}
+             ,{title: 'Bird', stack:['React.js', 'Django', 'REST'], img: 'https://media.giphy.com/media/cMEcAd8nS8UAQAdtLi/giphy.gif', url: 'https://github.com/Rezxx/Bird', desc: 'Web music player based on Django and React, syncing Spotify and Youtube account'}
+             ,{title: 'Digger', stack:['Datalog'], img: face, url: 'https://bitbucket.org/Rex9611/info3600-bugchecker/src/master/', desc: 'Static analysis tool finding Java Null Pointer Exceptions based on Soufflé and Doop'}
+             ,{title: 'ClearPay', stack:['Java', 'Android'], img: 'https://media.giphy.com/media/RNWZ0rqJOLdGfWXG3R/giphy.gif', url: '#', desc:'An Android App for splitting bills between users, supporting offline transactions via Bluetooth.'}];
+
+const colours = ["magenta", "red", "volcano", "orange", "gold", "lime", "green", "cyan", "blue", "geekblue", "purple", "processing"];
+var stack = new Set();
+projects.forEach(p =>{
+  p.stack.forEach(s =>{
+    stack.add(s);
+  });
+});
+
+var maps = {};
+[...stack].forEach((s, i) => {maps[s] = colours[i];console.log(s, i);});
 
 const Thumbnail = ({ project, i }) => {
   const odd = !(i & 1);
@@ -44,11 +58,13 @@ const Thumbnail = ({ project, i }) => {
       />
     </a>
   </motion.div>;
-
-  const text = <div key='text' style={{margin:'0 20px', minWidth: '15vw'}}>
+  console.log(project);
+  const text = <div key='text' style={{padding:'0 20px', minWidth: '15vw', maxWidth: '15vw'}}>
       <div className='porject_name'>
         <a href={project.url}>{project.title}</a>
-      </div>{project.desc}
+      </div>
+      {project.desc}
+      <div style={{lineHeight:'30px'}}><b>Stack:</b> {project.stack.map(stack => <Tag key={stack} color={maps[stack]}>{stack}</Tag>)}</div>
     </div>;
 
   return(
